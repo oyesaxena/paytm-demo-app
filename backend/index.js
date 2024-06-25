c8f43d18205b56130c336517c2299d8150d94814
Abhishek Saxena2 <abhisheksaxena2@M-C56R6F7MCW.local>
2024-06-26 02:50:01 +0530

    yarning build

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const rootRouter = require('./routes/index');
+import userRouter from './routes/user';
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+app.use('/api/v1', rootRouter);
+
+app.use('/api/v1/user', userRouter);
+
+app.listen(3000);
